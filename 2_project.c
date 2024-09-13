7072405a3196f17d0f964d501520696867c9886f
SJin05 <mozi_07@naver.com>
2024-09-13 21:00:11 +0900

    Add files via upload

--- 2_project.c
+++ 2_project.c
@@ -7,25 +7,34 @@ int main() {
     int plus[MAX_ITEM] = {0};
     int min[MAX_ITEM] = {0};
     int num_id[MAX_ITEM];
-    int id;
+    int total_item = 0;
+    int total_sold = 0;
+    double total_sold_rate = 0.0;
+    int max_sold, min_sold = 0;
+    int max_id, min_id = 0;
+    
 
-    printf("상품의 종류를 입력해주세요:");
+
+    printf("상품의 개수를 입력해주세요: ");
     scanf("%d", &item_num);
 
     if (item_num < 1 || item_num > 100){
-        printf("1에서 100 사이의 값을 입력해주세요:\n");
+        printf("1에서 100 사이의 값을 입력해주세요.\n");
         return 1;
     }
-
-    for (int i = 0; i < item_num; i++){
+    //상품의 개수에 따라 상품 ID 설정
+    for (int i = 0; i < item_num; i++){ 
         num_id[i] = i + 1;
     }
 
-    printf("\n상품들의 입고 수량을 입력해주세요:\n");
+
+    printf("\n상품 별 입고수량을 입력해주세요:\n");
     for (int i = 0; i < item_num; i++) {
         scanf("%d", &plus[i]);
         item[i] = plus[i];
+        total_item += plus[i];
     }
+
 
     printf("\n상품들의 판매 수량을 입력해주세요:\n");
     for (int i = 0; i < item_num; i++){
@@ -35,23 +44,50 @@ int main() {
             printf("판매 수량이 재고보다 많습니다. 다시 입력해주세요\n");
         } else {
             item[i] -= min[i];
+            total_sold += min[i];
+        }
+    } 
+
+
+    max_sold = min[0];
+    min_sold = min[0];
+    max_id = min_id = num_id[0];
+
+    for (int i = 1; i < item_num; i++) {
+
+        if (min[i] > max_sold) {
+        max_sold = min[i];
+        max_id = num_id[i];
+        }
+
+        if (min[i] < min_sold) {
+        min_sold = min[i];
+        min_id = num_id[i];
         }
     }
 
-    printf("\n확인할 상품의 ID를 입력하세요: ");
-    scanf("%d", &id);
-
-    if (id < 1 || id > item_num){
-        printf("올바른 상품 ID를 입력해주세요.\n");
-        return 1;
+    if (total_item > 0) {
+        total_sold_rate = (double)total_sold / total_item * 100;
     }
-    printf("상품 ID %d의 재고 수량: %d\n", id, item[id - 1]);
 
-    printf("\n각 상품의 재고 수량: ");
+
+    printf("\n재고 수량: ");
         for (int i = 0; i < item_num; i++) {
             printf("%d ", item[i]);
     }
 
-    return 0;
 
+    printf("\n총 판매량: %d (판매율 %.2f%%)", total_sold, total_sold_rate);
+
+
+    printf("\n가장 많이 판매된 상품: ID %d, 판매량 %d", max_id, max_sold);
+    printf("\n가장 적게 판매된 상품: ID %d, 판매량 %d", min_id, min_sold);
+
+
+    for (int i = 0; i < item_num; i++) {
+        if (item[i] <= 2) {
+            printf("\n상품 ID %d: 재고부족(%d)", num_id[i], item[i]);
+        }
+    }
+    return 0;
 }
