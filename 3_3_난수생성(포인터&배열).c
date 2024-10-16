64324309c38e25c454e243eddebd5444af422ed7
SJin05 <mozi_07@naver.com>
2024-10-16 14:42:36 +0900

    Update and rename 3_03.c to 3_3_난수생성(포인터&배열).c

--- 3_3_난수생성(포인터&배열).c
+++ 3_3_난수생성(포인터&배열).c
@@ -3,9 +3,11 @@
 #include <time.h>
 #define SIZE 100
 
+//배열에 임의의 값 입력함수
 void inputRandomArray(int* array, int size){
     for(int i = 0; i < size; i++){
         *(array+i) = rand()%100;
+        //*(array+i) = array[i]
     }
 
 }
@@ -13,7 +15,7 @@ void inputRandomArray(int* array, int size){
 void printArray(int* array, int size){
     for(int i = 0; i < size; i++){
         printf("%3d\t", array[i]);
-        if((i+1) % 10 == 0) printf("\n");
+        if((i+1) % 10 == 0) printf("\n"); //10개씩 출력되도록 줄바꿈
     }
 }
 
