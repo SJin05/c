1a007d288105fe926e385bc847730c89b459268c
SJin05 <mozi_07@naver.com>
2024-10-16 14:31:59 +0900

    Update and rename 1_04.c to 1_4_배열의정렬.c

--- 1_4_배열의정렬.c
+++ 1_4_배열의정렬.c
@@ -1,3 +1,4 @@
+//큰수 -> 작은수
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -13,12 +14,14 @@ int main(){
         number[i] = (rand() % 10000) +1;
     }
 
+    //수 비교
     for(int i=0; i<SIZE-1; i++){
-        most = i;
+        most = i; //처음 수
         for(int j=i+1; j<SIZE; j++){
             if(number[most] < number[j])
-                most = j;
+                most = j; //다음 수 
         }
+        //수 교환
         temp = number[i];
         number[i] = number[most];
         number[most] = temp;
