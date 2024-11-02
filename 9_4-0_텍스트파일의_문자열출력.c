4ba43af7672dcfebf3b119a6e79dc34cb72513e7
SJin05 <mozi_07@naver.com>
2024-11-03 02:43:49 +0900

    Update and rename 9_4-0_텍스트2.c to 9_4-0_텍스트파일의_문자열출력.c

--- 9_4-0_텍스트파일의_문자열출력.c
+++ 9_4-0_텍스트파일의_문자열출력.c
@@ -19,7 +19,7 @@ int main()
         return 0;
     }
     while(!feof(fp)){
-        fgets(input, 100, fp);
+        fgets(input, 100, fp); //파일에서 최대 99자까지 읽어서 input 배열에 저장
         printf("%s", input);
         //우리가 원하는 작업 가능
     }
