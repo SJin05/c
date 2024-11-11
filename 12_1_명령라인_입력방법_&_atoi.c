3d5e9bf31c5119a0c93713cab990b1d1df1bc3cc
SJin05 <mozi_07@naver.com>
2024-11-11 22:47:01 +0900

    Add files via upload

---
+++ 12_1_명령라인_입력방법_&_atoi.c
@@ -0,0 +1,25 @@
+//첫번째 코드
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    for(int i=0; i<argc; i++){
+        puts(argv[i]);
+    }
+    return 0;
+}
+
+
+
+//2번째 코드
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+    int a, b; 
+
+    a = atoi(argv[1]); //atoi -> 숫자형식 문자를 숫자로 바꿈
+    b = atoi(argv[2]);
+
+    printf("a+b = %d", a+b);
+    return 0;
+}
---
+++ 12_2_소스코드관리.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "12_2_calculator.h"
+
+int main(){
+    printf("%d", printAdd(10, 20));
+    return 0;
+}
