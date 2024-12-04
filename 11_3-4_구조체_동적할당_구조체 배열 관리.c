0cd4c27b553513d970e6d70fa0068436e2a871d6
SJin05 <mozi_07@naver.com>
2024-12-04 23:12:26 +0900

    Update and rename 11_3-4_구조체_동적할당_응용.c to 11_3-4_구조체_동적할당_구조체 배열 관리.c

--- 11_3-4_구조체_동적할당_구조체 배열 관리.c
+++ 11_3-4_구조체_동적할당_구조체 배열 관리.c
@@ -24,21 +24,21 @@ int main(){
         menu = inputMenu();
         if(menu == 1){
             //구조체 만들기
-            points[count] = (POINT*)malloc(sizeof(POINT));
-            points[count]->x = rand() % 100;
-            points[count]->y = rand() % 100;
+            points[count] = (POINT*)malloc(sizeof(POINT)); //동적 메모리 할당
+            points[count]->x = rand() % 100; //x 좌표 랜덤 값
+            points[count]->y = rand() % 100; //y 좌표 랜덤 값
             count++;
             printf("잘 만들었습니다.\n");
         }else if(menu == 2){
             //구조체 출력하기
             for(int i = 0; i<count; i++)
-                printf("x:%d, y:%d\n", points[i]->x, points[i]->y);
+                printf("x:%d, y:%d\n", points[i]->x, points[i]->y); // 저장된 구조체 출력
         }else if(menu == -1){
             printf("프로그램을 종료합니다.\n");
             for(int i = 0; i<count; i++)
                 free(points[i]);
         }
-    }while(menu > 0);
+    }while(menu > 0); // -1 입력 시 종료
 
     return 0;
 }
