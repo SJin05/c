ae20c7107d21ce256a48050b268a59e85ea80042
SJin05 <mozi_07@naver.com>
2024-12-04 21:25:10 +0900

    Update 10_2_fwrite.c

--- 10_2_fwrite.c
+++ 10_2_fwrite.c
@@ -1,17 +1,17 @@
-//fwrite
+//fwrite -> 바이너리 데이터를 파일에 기록
 
 #include <stdio.h>
 #define SIZE 100
 
 int main(){
-    char input[SIZE];
+    char input[SIZE]; //사용자로부터 입력받을 문자열을 저장할 배열
     FILE* fp = NULL;
 
     if((fp=fopen("output.bin", "wb")) == NULL){
         printf("error...");
         return 0;
     }
-    gets(input);
+    gets(input); //사용자로부터 문자열을 입력받음
     //fputs(input, fp);
 
     fwrite(input, strlen(input), 1, fp);
