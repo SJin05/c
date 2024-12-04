17c004c83aa5fda45cd9b98843e521d3f20b6ca5
SJin05 <mozi_07@naver.com>
2024-12-04 22:38:23 +0900

    Update and rename 11_2_구조체배열_동적할당.c to 11_2_구조체 배열 동적 생성.c

--- 11_2_구조체 배열 동적 생성.c
+++ 11_2_구조체 배열 동적 생성.c
@@ -11,9 +11,9 @@ typedef struct point{
 int main(){
     POINT* points;
     int count;
-    scanf("%d", &count);
+    scanf("%d", &count); //사용자가 구조체 배열의 크기를 입력
     
-    points = (POINT*)malloc(sizeof(POINT) * count);
+    points = (POINT*)malloc(sizeof(POINT) * count); //입력된 크기만큼 메모리를 할당 -> POINT 구조체 하나의 크기
 
     for(int i = 0; i<count; i++){
         points[i].x = rand() % 100;
@@ -25,3 +25,11 @@ int main(){
     free(points);
     return 0;
 }
+
+//5
+
+//x - 23, y - 45
+//x - 67, y - 12
+//x - 89, y - 34
+//x - 12, y - 76
+//x - 43, y - 98
